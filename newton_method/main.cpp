716403db3faf12214b5e08ff6e973783d3d3047b
korhanreb <48060375+korhanreb@users.noreply.github.com>
2019-03-03 13:45:01 +0100

    newton_method

---
+++ newton_method/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+
+double new_x(double num, double x)
+{
+    double func = x * x -num;
+    double func_der = 2 * x;
+   
+    return x - func / func_der;
+}
+
+double sqrt_newton(double num, double x0)
+{
+    double stop = 1E-5;
+    double x1;
+    do
+    {
+        x1 = x0;
+        x0 = new_x(num, x0);
+    }while(std::abs(x1 - x0) >= stop);
+    
+    return x1;
+}
+
+
+int main()
+{
+    std::cout.precision(16);
+    
+    std::cout << sqrt_newton(52, 4) << std::endl;
+
+    return 0;
+}
+
