0e5c880606da07866eae115df1b4dbf6cf410f7b
korhanreb <48060375+korhanreb@users.noreply.github.com>
2019-04-21 20:20:52 +0200

    Add files via upload

--- idomeres/main.cpp
+++ idomeres/main.cpp
@@ -4,48 +4,59 @@
 #include <random>
 #include <chrono>
 #include <fstream>
+#include <algorithm>
+
+double f()
+    { 
+        std::random_device rd{};
+        std::mt19937 gen(rd());
+        std::normal_distribution<double> distr(100, 20);  //átlag, szórás
+        return distr(gen);
+    }
 
 
 int main(int, char**) {
 
 
-std::ofstream myfile("data.txt", std::ios::out | std::ios::app);
+std::ofstream myfile("data.txt");
 
 
 for (int j=0; j<100; ++j)  //dim megadása
 {
     matrix<double> A(j);
-    matrix<double> B(j);
-    for(int i=0; i<j*j; ++i) //A feltöltése
-    {
-        std::random_device rd{};
-        std::mt19937 gen(rd());
-        std::normal_distribution<double> distr(100, 20);  //átlag, szórás
-        A[i]=distr(gen);
-    }
-    for(int i=0; i<j*j; ++i) //B feltöltése
-    {
-        std::random_device rd{};
-        std::mt19937 gen(rd());
-        std::normal_distribution<double> distr(100, 20);  //átlag, szórás
-        B[i]=distr(gen);
-    }
+    matrix<double> B(j);   
 
-    auto t1 = std::chrono::high_resolution_clock::now();
-    A*B;
-    auto t2 = std::chrono::high_resolution_clock::now();
     
-    auto t3 = std::chrono::high_resolution_clock::now();
-    std::move(A)*B;
-    auto t4 = std::chrono::high_resolution_clock::now();
+    std::generate(A.begin(), A.end(), f);
+    std::generate(B.begin(), B.end(), f);
+
+
+    //std:: cout << A[0] << std:: endl;
+    //std:: cout << B(0,0) << std:: endl;
+
+    int N{5};  //hányszor végezzük el az időmérést
+    std::vector<long long> T1(N);
+    std::vector<long long> T2(N);
+
+    for (int i=0; i<N; ++i)
+    {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        A*B;
+        auto t2 = std::chrono::high_resolution_clock::now();
+    
+        auto t3 = std::chrono::high_resolution_clock::now();
+        std::move(A)*B;
+        auto t4 = std::chrono::high_resolution_clock::now();
    
     
-    long long n1 = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();
-    long long n2 = std::chrono::duration_cast<std::chrono::nanoseconds>(t4-t3).count();
+        T1[i] = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();
+        T2[i] = std::chrono::duration_cast<std::chrono::nanoseconds>(t4-t3).count();
+    }
     
 
-    
-    myfile << j << ", " << n1 << ", " << n2  << ", \n";
+    auto min_value1 = *std::min_element(T1.begin(),T1.end());
+    auto min_value2 = *std::min_element(T2.begin(),T2.end());
+    myfile << j << ", " << min_value1 << ", " << min_value2  << ", \n";
     
 
 }
